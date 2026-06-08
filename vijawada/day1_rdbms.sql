c16bf40d97bd908a9f23cf1536f4836f60759e9e
67VINI67 <srivinishruthan.a@hcltech.com>
2026-06-08 06:34:38 +0000

    addd

---
+++ vijawada/day1_rdbms.sql
@@ -0,0 +1,43 @@
+create table emp_data(
+  empid varchar(50) not null,
+  empname text,
+  email text,
+  SAPID varchar(20) not null,
+  dep_name text,
+  mobile_no int,
+  loc text,
+  primary key (empid)
+);
+
+create table dep_data(
+  depid varchar(50),
+  depname varchar(50),
+  building varchar(50),
+  floor_name varchar(50),
+  primary key (depid)
+);
+
+create table emp_dep_data(
+  id varchar(50),
+  emp_id varchar(50),
+  dep_id varchar(50),
+  DOJ varchar(50),
+  foreign key (emp_id) references emp_data(empid),
+  foreign key (dep_id) references dep_data(depid),
+  primary key (id)
+);
+insert into emp_data values(78647,"Tejesh","tejes@gmail.com",21398822,"DevOps",987463212,"Japan");
+insert into emp_data(empid,empname,SAPID) values(52363798,"Balasack",43240932);
+insert into emp_data(empid,empname,SAPID) values(52363777,"Vini",432421);
+insert into emp_data(empid,empname,SAPID) values(52363799,"9a9a",497282);
+
+alter table emp_data add gender text;
+
+update emp_data set gender="Male" where empid=78647;
+update emp_data set gender="Male" where empid=52363798; 
+
+select * from emp_data;
+select * from emp_data where gender="Male";
+
+alter table emp_data drop column email;
+select * from emp_data;
