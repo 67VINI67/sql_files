cae4d9ad84bead7ccb42ca3ac67569722a36fd2a
67VINI67 <srivinishruthan.a@hcltech.com>
2026-06-10 17:21:21 +0530

    Update sql_query_for_comparing.sql

--- sql_query_for_comparing.sql
+++ sql_query_for_comparing.sql
@@ -1,1 +1,4 @@
 select ename,empno,sal,grade from emp,salgrade where sal between losal and hisal;
+
+-- this can also be used
+select ename,empno,sal,grade from emp,salgrade where sal >= losal and sal<= hisal;
