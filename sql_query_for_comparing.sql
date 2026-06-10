57330bf5f4d2beeb0ff7dceb5caf4a946a5f39ec
67VINI67 <srivinishruthan.a@hcltech.com>
2026-06-10 17:57:46 +0530

    Update sql_query_for_comparing.sql

--- sql_query_for_comparing.sql
+++ sql_query_for_comparing.sql
@@ -9,8 +9,13 @@ select ename,empno,sal,grade from emp join salgrade on sal >= losal and sal<= hi
 --this is also another code according to ANSI standard
 select ename,empno,sal,grade from emp join salgrade on sal between losal and hisal;
 
---this is left join, dept table will be fully there, coz it is in the left size in the query and it is a left join query
+--this is left join, dept table will be fully there, coz it is in the left side in the query and it is a left join query
 select ename,empno,emp.deptno,dept.deptno,dname from emp,dept where emp.deptno(+)=dept.deptno;
+
+--this is right join, emp table will be fully there, coz it is in the right side in the query and it is a right join query
+select ename,empno,emp.deptno,dept.deptno,dname from emp,dept where emp.deptno=dept.deptno(+);
+
 
 --this code is to change(remove or add) the constraint
  alter table emp drop constraint fk_deptno;
+
