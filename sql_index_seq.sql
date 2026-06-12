5f9754ed4f4862c1da4d4ff29ef4553ee3caab11
67VINI67 <srivinishruthan.a@hcltech.com>
2026-06-12 17:29:55 +0530

    Update sql_index_seq.sql

--- sql_index_seq.sql
+++ sql_index_seq.sql
@@ -1,3 +1,4 @@
+--this code is to create the sequence algorithm
 CREATE SEQUENCE seq1
     INCREMENT BY 1
     START WITH 1004
@@ -7,10 +8,11 @@ CREATE SEQUENCE seq1
     CACHE 5;
 
 SELECT * FROM USER_SEQUENCES;
- 
+ --dual is a dummy table, dual is used coz it is mandatory to use from keyword
 select seq1.NEXTVAL from dual;
  
 select seq1.CURRVAL from dual;
- 
+
+--this code is to insert the sequence value, like the current sequence value in to a table
 INSERT INTO EMP VALUES(seq1.NEXTVAL, 'Jaya', 'Trainee', 7782, sysdate, 5000, 100, 20);
  
